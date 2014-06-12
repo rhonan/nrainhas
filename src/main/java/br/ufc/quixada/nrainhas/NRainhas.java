2b94bffe5aa06f0674cb1fd6ecd06d0bde5c7767
rhonan <rhonanc@gmail.com>
2014-06-11 23:31:44 -0300

    Refatoração.

--- src/main/java/br/ufc/quixada/nrainhas/NRainhas.java
+++ src/main/java/br/ufc/quixada/nrainhas/NRainhas.java
@@ -5,136 +5,116 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
-/**
- * Converts NQueens problem into CNF for a particular value of n.
- * @author mon
- *
- */
 public class NRainhas {
-	public static int[][] board;
+	public static int[][] tabuleiro;
 	public static int n;
-	public static int variables;
-	public static int clauses;
+	public static int variaveis;
+	public static int clausulas;
 	public static StringBuilder builder;
-	public static String cnfFileName;
+	public static String nomeArquivoCNF;
 	
 	
     public static void main(String[] args){
     	
-    	Scanner scan=new Scanner(System.in);
-        System.out.println("Entre com o valor de N: ");
-        n=scan.nextInt();
-        scan.close();
-		// init the board
-		board = new int[n][n];
-		// label logical variables on the board
-		labelBoard(n);
-		// init clause builder for cnf
+    	if(args.length == 0){
+        	Scanner scan=new Scanner(System.in);
+            System.out.println("Entre com o valor de N: ");
+            n=scan.nextInt();
+            scan.close();
+    	}
+    	else{
+    		n=Integer.parseInt(args[0]);
+    	}
+		tabuleiro = new int[n][n];
+		preencherTabuleiro(n);
 		builder = new StringBuilder();
-		cnfFileName = n+"rainhas.cnf";
-		generateCNF();
+		nomeArquivoCNF = n+"rainhas.cnf";
+		gerarCNF();
 
     }
 
-    
-
-	/**
-	 * Label the board 1..n^2 where n is the desired dimension 
-	 * (size of the problem).
-	 * @param n : size of the problem. 
-	 */
-	public static void labelBoard(int n) {
+	public static void preencherTabuleiro(int n) {
 		int label = 1;
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				board[i][j] = label;
+				tabuleiro[i][j] = label;
 				label++;
 			}
 		}
-		variables = --label;
+		variaveis = --label;
 	}
-	/**
-	 * Generates a cnf String corresponding to the problem (with a particular 
-	 * value of n).
-	 * @return: StringBuilder containing cnf.
-	 */
-	public static StringBuilder generateCNF() {
-		generateRowClauses();
-		generateColumnClauses();
-		generateDiagonalClauses();
-		cnfToFile();
+	
+	public static StringBuilder gerarCNF() {
+		gerarClausulasLinhas();
+		gerarClausulasColunas();
+		gerarClausulasDiagonal();
+		gerarArquivoCNF();
 		return builder;
 	}
 
-	public static  void generateRowClauses() {
-		String clause = "";
-		int[] row = new int[n];
+	public static  void gerarClausulasLinhas() {
+		String clausula = "";
+		int[] linha = new int[n];
 		int k = 0;
 		for(int i=0; i<n; i++) {
-			// construct statement of form (x1 or x2 or... or xn)
 			for(int j=0; j<n; j++) {
-				row[k] = board[i][j];
-				clause += board[i][j] +" ";
+				linha[k] = tabuleiro[i][j];
+				clausula += tabuleiro[i][j] +" ";
 				k++;
 			}
-			appendClause(clause);
-			clause = "";
-			// construct statements of form (!x1 or !x2); (!x1 or !x3); (!x1 or !x2) etc.
+			addClausula(clausula);
+			clausula = "";
 			for(int l=0; l<n; l++) {
 				for(int m=l+1; m<n; m++) {
-					clause += "-"+row[l]+" -"+row[m]+" ";
-					appendClause(clause);
-					clause = "";
+					clausula += "-"+linha[l]+" -"+linha[m]+" ";
+					addClausula(clausula);
+					clausula = "";
 				}
 			}
 
 			k = 0;
 		}
 	}
-	public static void generateColumnClauses() {
-		String clause = "";
-		int[] column = new int[n];
+	public static void gerarClausulasColunas() {
+		String clausula = "";
+		int[] coluna = new int[n];
 		int k = 0;
 		for(int i=0; i<n; i++) {
-			// construct statement of form (x1 or x2 or... or xn)
 			for(int j=0; j<n; j++) {
-				column[k] = board[j][i];
-				clause += board[j][i] +" ";
+				coluna[k] = tabuleiro[j][i];
+				clausula += tabuleiro[j][i] +" ";
 				k++;
 			}
-			appendClause(clause);
-			clause = "";
-			// construct statements of form (!x1 or !x2); (!x1 or !x3); (!x1 or !x2) etc.
+			addClausula(clausula);
+			clausula = "";
 			for(int l=0; l<n; l++) {
 				for(int m=l+1; m<n; m++) {
-					clause += "-"+column[l]+" -"+column[m]+" ";
-					appendClause(clause);
-					clause = "";
+					clausula += "-"+coluna[l]+" -"+coluna[m]+" ";
+					addClausula(clausula);
+					clausula = "";
 				}
 			}
 			k = 0;
 		}
 	}
-	public static void generateDiagonalClauses() {
-		String clause = "";
-		// bottom-up left right part
+	public static void gerarClausulasDiagonal() {
+		String clausula = "";
 		int[] diagonal = new int[n];
-		// index for diagonal
 		int k = 0;
 		int l = 0;
 		for(int i=1; i<n*2-2; i++) {
 			for(int j = 0; j<=i; j++) {
 				k = i - j;
 				if(k<n && j< n) {
-					diagonal[l] = board[k][j];
+					diagonal[l] = tabuleiro[k][j];
 					l++;
 				}
 			}
 			for(int p=0; p<l; p++) {
 				for(int q=p+1; q<l; q++) {
-					clause += "-"+diagonal[p]+" -"+diagonal[q]+" ";
-					appendClause(clause);
-					clause = "";
+					clausula += "-"+diagonal[p]+" -"+diagonal[q]+" ";
+					addClausula(clausula);
+					clausula = "";
 				}
 			}
 			l = 0;
@@ -144,16 +124,16 @@ public class NRainhas {
 			for(int j = i+n-1; j>=0; j--) {
 				k = i + j;
 				if(k<n && j< n) {
-					diagonal[l] = board[k][j];
+					diagonal[l] = tabuleiro[k][j];
 					l++;
 				}
 		 }
 
 		for(int p=0; p<l; p++) {
 			for(int q=p+1; q<l; q++) {
-				clause += "-"+diagonal[p]+" -"+diagonal[q]+" ";
-				appendClause(clause);
-				clause = "";
+				clausula += "-"+diagonal[p]+" -"+diagonal[q]+" ";
+				addClausula(clausula);
+				clausula = "";
 			}
 		}
 		l = 0;
@@ -165,7 +145,7 @@ public class NRainhas {
 		while(iter>=2) {
 			iter = 0;
 			while(j<n) {
-					diagonal[l] = board[i][j];
+					diagonal[l] = tabuleiro[i][j];
 					j++;
 					i++;
 					iter++;
@@ -173,9 +153,9 @@ public class NRainhas {
 			}
 			for(int p=0; p<l; p++) {
 				for(int q=p+1; q<l; q++) {
-					clause += "-"+diagonal[p]+" -"+diagonal[q]+" ";
-					appendClause(clause);
-					clause = "";
+					clausula += "-"+diagonal[p]+" -"+diagonal[q]+" ";
+					addClausula(clausula);
+					clausula = "";
 				}
 			}
 			l = 0;
@@ -185,33 +165,29 @@ public class NRainhas {
 			if(j==n-1) break;
 		}
 	}
-	public static void appendClause(String clause) {
-		builder.append(clause);	
-		// AND
+	
+	public static void addClausula(String clausula) {
+		builder.append(clausula);	
 		builder.append("0\n");
-		clauses++;
+		clausulas++;
 	}
+	
 	/**
-	 * Writes cnf to file.
+	 * Adiciona a CNF ao arquivo.
 	 */
-	public static void cnfToFile()  {
+	public static void gerarArquivoCNF()  {
 		PrintWriter writer = null;
 		try {
-			writer = new PrintWriter(cnfFileName, "UTF-8");
+			writer = new PrintWriter(nomeArquivoCNF, "UTF-8");
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		// adhere to cnf convension
-		writer.println("c logic in conjunctive normal form");
-		writer.println("c for n-queens problem, with n = "+n);
-		writer.println("p cnf "+variables+" "+clauses);
+		writer.println("p cnf "+variaveis+" "+clausulas);
 		String[] cnfContent = builder.toString().split("\\n");
-		for(String clause:cnfContent) {
-			writer.println(clause);
+		for(String clausula:cnfContent) {
+			writer.println(clausula);
 		}
 		writer.close();
 	}
-	public static String getCnfFilename() {
-		return cnfFileName;
-	}
+	
 }
